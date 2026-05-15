53217cdfc9194bdab18f54abb20c83b7c908dd1b
BigStar0404 <1964667995@qq.com>
2026-05-15 22:40:10 +0800

    连接上site and game

--- apps/game/src/App.jsx
+++ apps/game/src/App.jsx
@@ -4,6 +4,18 @@ import './App.css'
 function App() {
   return (
     <main className="app-shell">
+      <a className="home-link" href="/" aria-label="Return to homepage">
+        <svg
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+          focusable="false"
+          className="home-link-icon"
+        >
+          <path d="M10 19l-7-7 7-7" />
+          <path d="M4 12h16" />
+        </svg>
+        <span>Home</span>
+      </a>
       <CanvasScene />
     </main>
   )
